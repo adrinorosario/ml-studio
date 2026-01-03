b7ce24fe1a138b070fbc267cbf33f0dbd84e5e17
adrinorosario <rosarioadrino@gmail.com>
2026-01-03 20:52:47 +0530

    Created project directory structure, added starting java files, the docker file, and other documentation files

---
+++ src/main/java/com/adrino/aurea_ml_studio/bootstrap/ApplicationBootstrap.java
@@ -0,0 +1,15 @@
+package src.main.java.com.adrino.aurea_ml_studio.bootstrap;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ApplicationBootstrap extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String []args) {
+
+    }
+}
---
+++ src/main/java/com/adrino/aurea_ml_studio/entrypoint/AppMain.java
@@ -0,0 +1,9 @@
+package src.main.java.com.adrino.aurea_ml_studio.entrypoint;
+
+import src.main.java.com.adrino.aurea_ml_studio.bootstrap.ApplicationBootstrap;
+
+public class AppMain {
+    public static void main(String []args) {
+        ApplicationBootstrap.launch(args);
+    }
+}
